523bc1dd4b2352ca7247c08c1abe920ac3b5bd0f
mmk <mmkkarbalaei@gmail.com>
2024-02-10 23:43:39 +0330

    create base structure of project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <cmath>
+#include <random>
+#include <iomanip> 
+
+using namespace std;
+using namespace std::chrono;
+
+int main(){
+    return 0;
+}
