67b36c09c184f8b0c9b07c9179ad83f146bf9f9b
mmk <mmkkarbalaei@gmail.com>
2024-02-10 23:46:47 +0330

    hillClimbing function

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 #include <chrono>
 #include <cmath>
 #include <random>
-#include <iomanip> 
+#include <iomanip>
 
 using namespace std;
 using namespace std::chrono;
 
 #define Filepath "\\dataset\\"
 
+void hillClimbing(double* Lands, double runtime, int DataSize, double totalLands) {
+}
 
-int main(){
+int main()
+{
     string filename = "1";
     double runtime = 5;
 
     ifstream file(Filepath + filename + ".txt");
-    if (!file) {
+    if (!file)
+    {
         cout << "Cannot open file: " << filename << endl;
         return 1;
     }
@@ -25,21 +29,28 @@ int main(){
     double totalLands = 0;
     double number;
 
-    while (file >> number) {
+    while (file >> number)
+    {
         DataSize++;
         totalLands += number;
     }
 
-    cout << endl << "Dataset " << filename << " with " << DataSize << " records " << endl << endl;
+    cout << endl
+         << "Dataset " << filename << " with " << DataSize << " records " << endl
+         << endl;
 
-    double* Lands = new double[DataSize];
+    double *Lands = new double[DataSize];
 
     file.clear();
     file.seekg(0, ios::beg);
 
-    for (int i = 0; i < DataSize; i++) {
+    for (int i = 0; i < DataSize; i++)
+    {
         file >> Lands[i];
     }
     file.close();
+    hillClimbing(Lands, runtime, DataSize, totalLands);
+    delete[] Lands;
+
     return 0;
 }
