ea7f4e01eb40a66e2bb372cecf5fb9b7bbfc94ea
mmk <mmkkarbalaei@gmail.com>
2024-02-10 23:45:43 +0330

    getting data from dataset file

--- main.cpp
+++ main.cpp
@@ -21,6 +21,25 @@ int main(){
         cout << "Cannot open file: " << filename << endl;
         return 1;
     }
-    
+    int DataSize = 0;
+    double totalLands = 0;
+    double number;
+
+    while (file >> number) {
+        DataSize++;
+        totalLands += number;
+    }
+
+    cout << endl << "Dataset " << filename << " with " << DataSize << " records " << endl << endl;
+
+    double* Lands = new double[DataSize];
+
+    file.clear();
+    file.seekg(0, ios::beg);
+
+    for (int i = 0; i < DataSize; i++) {
+        file >> Lands[i];
+    }
+    file.close();
     return 0;
 }
