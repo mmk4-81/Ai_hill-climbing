387f69fb8c476c9d5bf846b9d2260c91731b7df8
mmk <mmkkarbalaei@gmail.com>
2024-02-10 23:48:09 +0330

    تعریف اولیه مقادیر و اطلاعات

--- main.cpp
+++ main.cpp
@@ -10,8 +10,30 @@ using namespace std;
 using namespace std::chrono;
 
 #define Filepath "\\dataset\\"
+#define BOY1 0
+#define BOY2 1
+#define GIRL 2
 
 void hillClimbing(double* Lands, double runtime, int DataSize, double totalLands) {
+     double idealboy1 = totalLands / 5 * 2;
+    double idealboy2 = totalLands / 5 * 2;
+    double idealgirl = totalLands / 5;
+    double ideal[3] = { idealboy1, idealboy2, idealgirl };
+    cout << fixed << setprecision(4);
+    cout << "total lands =>" << totalLands << endl;
+    cout << "Ideal for boy 1 => " << idealboy1 << endl;
+    cout << "Ideal for boy 2 => " << idealboy2 << endl;
+    cout << "Ideal for girl => " << idealgirl << endl;
+    cout << endl;
+
+    auto start_time = high_resolution_clock::now();
+
+    int* CurrentDistribution = new int[DataSize];
+    int* bestDistribution = new int[DataSize];
+
+    double* sum = new double[3]{ 0, 0, 0 };
+    double* bestsum = new double[3]{ 0, 0, 0 };
+
 }
 
 int main()
